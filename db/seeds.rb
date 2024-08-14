40b49bd932a0d19dd035d033693811d53ea811e0
Julia Krebs <152524036+jcrabs@users.noreply.github.com>
2024-08-14 16:57:15 +0200

    Completed the 'Yelp MVP' challenge.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create(name: "number 1", address: "Berlin", phone_number: "1", category: "french")
+Restaurant.create(name: "number 2", address: "Munich", phone_number: "2", category: "chinese")
+Restaurant.create(name: "number 3", address: "London", phone_number: "3", category: "italian")
+Restaurant.create(name: "number 4", address: "SF", phone_number: "4", category: "japanese")
+Restaurant.create(name: "number 5", address: "Tokyo", phone_number: "5", category: "belgian")
+puts "Created #{Restaurant.all.length} restaurants."
